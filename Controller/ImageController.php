a8121adb41f7674425cc077dde753c3ef06ee055
Toni Zeidler <toni.z@gmx.de>
2022-07-19 01:05:28 +0200

    IC refactor

--- Controller/ImageController.php
+++ Controller/ImageController.php
@@ -14,25 +14,15 @@ class ImageController implements Controller {
     private PDO $pdo;
 
     public function __construct(private DB $db) {
-        #$this->pdo = $this->db->getPDO();
-        echo '<pre>';
-        var_dump($db->getDNS());
-        echo '</pre>';
-
-
-        
-        $this->pdo = new PDO($db->getDNS(),$db->getUser(),$db->getPass());
-
-        // try {
-        //     $this->pdo = new PDO($this->dbe->getDNS(), $this->dbe->getUser(), $this->dbe->getPass(), [
-        //         PDO::ATTR_EMULATE_PREPARES => false,
-        //         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        //     ]);
-        //     echo $this->pdo;
-        //   } catch (PDOException $e) {
-        //     echo 'Probleme mit der MySQL Datenbankverbindung...';
-        //     die();
-        //   }
+        try {
+            $this->pdo = new PDO($db->getDNS(),$db->getUser(),$db->getPass(), [
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+          } catch (PDOException $e) {
+            echo 'Probleme mit der MySQL Datenbankverbindung...';
+            die();
+          }
     }
 
     public function fetchAll(): array | null {
@@ -41,12 +31,7 @@ class ImageController implements Controller {
         #$stmt = $this->pdo->execute($sql);
         $stmt->execute();
         $results = $stmt->fetchAll(PDO::FETCH_CLASS, ImageModel::class);
-        var_dump($results);
+        //var_dump($results);
         return $results;
     }
 }
-
-
-//$ic = new ImageController(new DB(DBE::MySQL));
-// $images =$ic->fetchAll();
-// var_dump($images);
--- index.php
+++ index.php
@@ -2,16 +2,19 @@
 require_once('View/inc/utils.php');
 require_once('Model/DB.php');
 require_once('Controller/ImageController.php');
-$dbe = DBE::MySQL;
-echo $dbe->value;
-$db = new DB($dbe);
-echo '<pre>';
-var_dump($db);
-echo '</pre>';
-$ic = new ImageController($db);
-$images =$ic->fetchAll();
+// $dbe = DBE::MySQL;
+// echo $dbe->value;
+// $db = new DB($dbe);
+// echo '<pre>';
+// var_dump($db);
+// echo '</pre>';
+// $ic = new ImageController($db);
+// $images =$ic->fetchAll();
 // var_dump($images);
 
+
+$ic = new ImageController(new DB(DBE::MySQL));
+$images =$ic->fetchAll();
 
 render('View/index.view.php', [
     'images' => $images
