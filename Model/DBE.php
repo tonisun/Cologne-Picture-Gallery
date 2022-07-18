448d75c5f08acbccb6049f03d64bc027a73303ea
Toni Zeidler <toni.z@gmx.de>
2022-07-18 17:50:17 +0200

    DB

--- Model/DBE.php
+++ Model/DBE.php
@@ -10,13 +10,36 @@ enum DBE : string {
     case PostgreSQL = 'pgsql';
     
 
-    public function getDNS(): string {    
+    public function getDNS(): string { 
+
+        $ini_data = parse_ini_file('../DB/config.ini', true);
+        print_r($ini_data['MySQL']);
+        echo $ini_data['MySQL']['dbname'];
+        
         return match ($this){
-            self::MySQL => 'mysql:host=127.0.0.1;dbname=hospital;charset=utf8',
+            self::MySQL => "mysql:host=".$ini_data['MySQL']['host'].";dbname=".$ini_data['MySQL']['dbname'].";charset=utf8",
             self::SQLite => 'sqlite:db/hospital.db',
+            self::PostgreSQL => '',
             default => '',
         };
     }
 
 
 }
+//- DNS alt
+$dbe = DBE::MySQL;
+echo '<pre>';
+echo DBE::MySQL->getDNS();
+echo '</pre>';
+
+echo '<pre>';
+print_r($dbe);
+echo '</pre>';
+
+echo '<pre>';
+echo $dbe->value;
+echo '</pre>';
+
+echo '<pre>';
+echo $dbe->name;
+echo '</pre>';
