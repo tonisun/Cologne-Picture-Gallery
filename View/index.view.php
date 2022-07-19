9a5a20208000ef8170b5236993916bbea3ce515f
Toni Zeidler <toni.z@gmx.de>
2022-07-19 02:12:39 +0200

    all right

--- View/index.view.php
+++ View/index.view.php
@@ -1,42 +1,55 @@
-<?php
-    include_once('inc/header.php');
-?>
+<!DOCTYPE html>
+<html lang="de">
 
-<div class="section">
-    <div class="container">
-        <h4>Meine Bilder</h4>
-    </div>
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="View/assets/images/favicon_red.ico" type="image/x-icon">
+    <link rel="stylesheet" href="View/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="View/assets/css/styles.css">
+    <link rel="stylesheet" href="View/assets/css/custom.css">
+    <link rel="stylesheet" href="View/assets/css/simple.css">
+    <title>MVC</title>
+</head>
 
-<?php
-echo '<pre>';
-var_dump($images[0]);
+<body>
+    <h1>Welcomme MVC</h1>
 
-print_r($images[0]->getId());
-echo '</pre>';
-
-?>
-
-<?php if(!empty($images)): ?>
-<div class="section">
-    <div class="container">
-        <div class="image-gallery">
-            <?php foreach($images AS $image): ?>
-            <figure class="image-gallery-item">
-                <img class="image-gallery-image" src="/View/assets/images/<?php echo $image->getFileName(); ?>"
-                    alt="Kerl1">
-                <figcaption><b><?php echo $image->getId(); ?>. <?php echo esc($image->getTitle()); ?></b>
-                    <?php echo esc($image->getDescription()); ?>
-                </figcaption>
-            </figure>
-            <?php endforeach; ?>
+    <div class="section">
+        <div class="container">
+            <h4>Meine Bilder</h4>
         </div>
     </div>
-</div>
-<?php else: ?>
-<p>Es wurden noch keine Bilder erfasst.</p>
-<?php endif; ?>
 
-<?php
+    <?php
+// echo '<pre>';
+// var_dump($images[0]);
+
+// print_r($images[0]->getId());
+// echo '</pre>';
+?>
+
+    <?php if(!empty($images)): ?>
+    <div class="section">
+        <div class="container">
+            <div class="image-gallery">
+                <?php foreach($images AS $image): ?>
+                <figure class="image-gallery-item">
+                    <img class="image-gallery-image" src="View/assets/images/<?php echo $image->getFileName(); ?>"
+                        alt="Kerl1">
+                    <figcaption><b><?php echo $image->getId(); ?>. <?php echo esc($image->getTitle()); ?></b>
+                        <?php echo esc($image->getDescription()); ?>
+                    </figcaption>
+                </figure>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+    <?php else: ?>
+    <p>Es wurden noch keine Bilder erfasst.</p>
+    <?php endif; ?>
+
+    <?php
     include_once('inc/footer.php');
 ?>
